5f41ea87dcaf14a1d87a7dedd345cd3390059666
metametadata <yuri.govor@gmail.com>
2015-09-11 18:44:02 +0300

    fixed comment

--- src/my/core.cljs
+++ src/my/core.cljs
@@ -8,6 +8,6 @@
 (let [v 123
       obj (clojure.core/reify
            Object
-           (meth [this x y & z] ;; remove ampersand and it script will run
+           (meth [this x y & z] ;; remove ampersand to make this script work
              [v x y z]))]
   (println (.meth obj 1 2 3)))
